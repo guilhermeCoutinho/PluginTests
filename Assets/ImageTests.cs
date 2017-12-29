95085eb5520cc6dcee0a3614b13ab26df2471d66
GuilhermeCoutinho <guilhermejcoutinho@gmail.com>
2017-12-29 16:52:55 -0200

    Initial commit

---
+++ Assets/ImageTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageTests : MonoBehaviour {
+
+    [HideInInspector]public Texture2D tex;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetTexture(Texture2D texture)
+    {
+        spriteRenderer.sprite = createSprite(texture);
+    }
+
+    Sprite createSprite(Texture2D tex)
+    {
+        return Sprite.Create(tex,
+        new Rect(0f, 0f, tex.width, tex.height),
+        new Vector2(.5f, .5f),
+        tex.height);
+    }
+
+}
---
+++ Assets/OpenFromGallery.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VoxelBusters.NativePlugins;
+
+public class OpenFromGallery : MonoBehaviour {
+
+    public Texture2D texture; 
+    public ImageTests imageTests;
+    public void Open()
+    {
+        #if UNITY_EDITOR
+        PickImageFinished(texture);
+        #else
+        NPBinding.MediaLibrary.SetAllowsImageEditing(false);
+        NPBinding.MediaLibrary.PickImage(eImageSource.ALBUM, 1, PickImageFinished);
+       #endif
+    }
+
+    private void PickImageFinished(
+        ePickImageFinishReason _reason, Texture2D _image)
+    {
+        if (_image == null)
+            return;
+        imageTests.SetTexture(_image);
+    }
+
+    private void PickImageFinished(Texture2D _image)
+    {
+        if (_image == null)
+            return;
+        imageTests.SetTexture(_image);
+    }
+}
