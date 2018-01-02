21ad6e526e4317c4b309569ea7d895eac803433c
GuilhermeCoutinho <guilhermejcoutinho@gmail.com>
2018-01-02 17:34:25 -0200

    Added Reign plugin

--- Assets/OpenFromGallery.cs
+++ Assets/OpenFromGallery.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using VoxelBusters.NativePlugins;
+using System.IO;
+using System;
+using Reign;
 
 public class OpenFromGallery : MonoBehaviour {
 
@@ -30,5 +33,35 @@ public class OpenFromGallery : MonoBehaviour {
         if (_image == null)
             return;
         imageTests.SetTexture(_image);
+    }
+
+    public void PickImage () {
+        StreamManager.LoadFileDialog(FolderLocations.Pictures, new string[]{".png", ".jpg"}, imageLoadedCallback);
+    }
+
+    private void imageLoadedCallback(Stream stream, bool succeeded)
+    {
+        if (!succeeded)
+        {
+            if (stream != null) stream.Dispose();
+            return;
+        }
+
+        try
+        {
+            var data = new byte[stream.Length];
+            stream.Read(data, 0, data.Length);
+            Texture2D tex = new Texture2D(4,4);
+            texture.LoadImage(data);
+            imageTests.SetTexture(texture);
+        }
+        catch (Exception e)
+        {
+            MessageBoxManager.Show("Error", e.Message);
+        }
+        finally
+        {
+            if (stream != null) stream.Dispose();
+        }
     }
 }
